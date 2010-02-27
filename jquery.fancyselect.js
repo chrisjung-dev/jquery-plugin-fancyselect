c5ca8fdfec3826eff5772580b6880bc61b12969b
Chris Jung <campino2k@gmail.com>
2010-02-27 12:53:02 +0100

    Made some tweaks and structural changes
    - dealing with optgroup instead of omittimg them

--- jquery.fancyselect.js
+++ jquery.fancyselect.js
@@ -5,16 +5,19 @@
  * @license CC-BY-SA
  *
  * Date: Fr 26 Feb 2010 20:24:52 CET 
+ *
+ * Tutorial for developing jQuery plugins
+ * http://www.learningjquery.com/2007/10/a-plugin-development-pattern
+ *
  */
 
 (function($) {
-
 	$.fn.FancySelect = function(options) {
 		opts = $.extend({}, $.fn.FancySelect.defaults, options);
 		return this.each(function(){
-			$this = $(this);
+			var $this = $(this);
 			$this.wrap('<div class="select_replace" id="'+$this.attr('name')+'_dropdown"></div>').hide();
-			$replace = $this.parents('.select_replace');
+			var $replace = $this.parents('.select_replace');
 			$replace.append('<div class="display">'+$this.find('option:selected').text()+'</div>')
 			$replace.click(function(){
 				$.fn.FancySelect.openList($replace);
@@ -24,31 +27,53 @@
 		
 		//$(document).click(close);
 	},
-
-	
 	$.fn.FancySelect.openList = function(_el){
-		$this=$(_el);
-		if( !$this.find('.options').length ) {
-			$this.append('<div class="options"><ul></ul></div>');
+
+		var $this=$(_el); // save element
+		var optgroups 	= $this.find('select optgroup');
+		var options 	= $this.find('select option');
+		var n_optgroups	= optgroups.length; //save number of optgroups
+		var n_options 	= options.length; //save number of options
+		var has_options = $this.find('.options').length; 
+
+		if( n_options > 0 && has_options == 0 ) {
+			//if optgroups only, loop is not needed, cause there is nothing clickable
+
+			$this.append('<div class="options"></div>');
+
+			var $list = $this.find('.options').empty();
+			var i_optgroups;
+
+			for( i_optgroups = 0; i_optgroups < n_optgroups; i_optgroups++ ) {
+				var new_group = $list.append('<div class="group"></div>');
+				if( $( optgroups[ i_optgroups ] ).attr('label') != "" ) {
+					$(new_group).append('<strong>' + $( optgroups[ i_optgroups ] ).attr('label') + '</strong>');
+				}
+				var o_options 	= $( optgroups ).find('option');
+				var o_n_options = o_options.length;
+				$.fn.FancySelect.renderOptions( o_options, o_n_options, new_group );
+			}
+			if( n_optgroups = 0 ) {
+				$.fn.FancySelect.renderOptions( options, n_options, $list );
+			}
 		} else if( $this.find('div.options:hidden').length ) {
 			$this.find('div.options').show();
 		} else {
 			$.fn.FancySelect.closeList();		
 		}
-
 	},
-	
-	
+	$.fn.FancySelect.renderOptions = function( _options, _n_options, _target ) {
+		var i_options;
+		for( i_options = 0; i_options < _n_options; i++ ) {
+			$(_target).append('<li value="' + $( _options[ i_options ]).attr('value') + '">'+ $( _options[ i_options ]).text() + '</li>');
+		}
+	},
 	$.fn.FancySelect.closeList = function(e){
 		$('div.options:visible').hide();
 	},
-	
 	// plugin defaults - added as a property on our plugin function
 	$.fn.FancySelect.defaults = {
-		borderColor: '#fd0000',
+		borderColor: '#ccc',
 		color: '#0000fd'
 	};
-
-	// http://www.learningjquery.com/2007/10/a-plugin-development-pattern
-
 })(jQuery);
